2ce8091573d04318b00852cace7df5d359fd3b75
Rogger Matamoros <roggmatz@gmail.com>
2017-04-24 20:11:31 -0400

    Registered Todo repository with DI service.

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
+using TodoAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace TodoAPI
 {
     public class Startup
@@ -27,8 +30,10 @@ namespace TodoAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase());
             // Add framework services.
             services.AddMvc();
+            services.AddScoped<ITodoRepository, TodoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
