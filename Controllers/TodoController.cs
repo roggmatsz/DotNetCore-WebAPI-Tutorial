3dfe3eb044b357d3c960f4c32d1665c4e2dbe7ad
Rogger Matamoros <roggmatz@gmail.com>
2017-04-24 23:08:38 -0400

    Comments for your health.

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -3,24 +3,36 @@ using Microsoft.AspNetCore.Mvc;
 using TodoAPI.Models;
 
 namespace TodoAPI.Controllers {
+    //localhost:port/api/todo/*
     [Route("api/[controller]")]
     public class TodoController : Controller {
         private readonly ITodoRepository _todoRepository;
+        
+        //constructor receives a reference to repository class,
+        //stores it in _todoRepository, which cannot be modified
+        //after it is set.
         public TodoController(ITodoRepository repository) {
             _todoRepository = repository;
+        }
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll() {
+            return _todoRepository.GetAll();
         }
         [HttpGet("{id}", Name = "GetTodo")]
         public IActionResult GetById(long id) {
             var item = _todoRepository.Find(id);
             if(item == null) {
-                return NotFound();
+                return NotFound(); //404
             }
+            //item is serialized by ObjectResult before being returned.
             return new ObjectResult(item);
         }
+
+        //FromBody means item object is found in body of Post request
         [HttpPost]
         public IActionResult Create([FromBody] TodoItem item) {
             if(item == null) {
-                return BadRequest();
+                return BadRequest(); //400
             }
             _todoRepository.Add(item);
             return CreatedAtRoute("GetTodo", new { id = item.Key}, item);
--- Models/ITodoRepository.cs
+++ Models/ITodoRepository.cs
@@ -1,5 +1,10 @@
 using System.Collections.Generic;
 
+//interface that enables multiple implementations of
+//persistence schemes and testing using repos with
+//mock data.
+//As long as the a repository class implements this 
+//interface aka. this set of methods, the system works.
 namespace TodoAPI.Models {
     public interface ITodoRepository {
         void Add(TodoItem item);
